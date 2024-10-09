f68ce5e701a5bd7ddb4c92f75b8928476b9c0877
sterlen7 <infosterlen@gmail.com>
2024-10-09 07:21:28 +0000

    cors

--- main.js
+++ main.js
@@ -6,13 +6,21 @@ const fs = require('fs');
 const path = require('path');
 const db_connection = require('./config/db');
 const setTokenCleanUp = require('./utils/cleanToken')
+const cors = require('cors')    
+
+const corsOptions = {
+    origin:['https://mincom-requisition.onrender.com', 'http://localhost:5000'],
+    optionsSuccessStatus: 200
+}
 
 
 
 db_connection();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
+
+app.use(cors(corsOptions))
 app.use(express.json());  
 app.use(express.urlencoded({ extended: true })); 
 
