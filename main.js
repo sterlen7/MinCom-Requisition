32acc5ace0e7d69d0483ebf740bbe6f928ab5a24
sterlen7 <infosterlen@gmail.com>
2024-11-23 21:07:32 +0000

    vercel

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const setTokenCleanUp = require('./utils/cleanToken')
 const cors = require('cors')    
 
 const corsOptions = {
-    origin:['https://mincom-requisition.onrender.com', 'http://localhost:5000', 'http://localhost:5173','https://vercel.com/'],
+    origin:['https://mincom-requisition.onrender.com', 'http://localhost:5000', 'http://localhost:5173','https://vercel.com/','https://mincom-frontend.vercel.app/'],
     optionsSuccessStatus: 200
 }
 
